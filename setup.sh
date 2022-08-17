1e216b8c1fa069deb78f3e84b87f75d76a6d1c06
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2022-08-17 16:38:12 -0400

    add battery modules

--- setup.sh
+++ setup.sh
@@ -69,6 +69,9 @@ function debian {
   #sudo apt update
   #sudo apt install --install-recommends kicad
 
+  echo -e "${CYAN}Copying modules to /usr/share/kicad...${NC}"
+  sudo cp -Rp modules /usr/share/kicad
+
   # Install KiBot
   if [ -d "KiBot" ]; then
     echo -e "${CYAN}Found Clone of KiBot Repo... skipping${NC}"
